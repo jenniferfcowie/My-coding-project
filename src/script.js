3933478948eab2ac614dc1a64730b7e930c64802
jenniferfcowie <81190247+jenniferfcowie@users.noreply.github.com>
2021-05-21 20:07:29 +0100

    Added getForecast function

--- src/script.js
+++ src/script.js
@@ -33,24 +33,27 @@ function formatDate(timestamp) {
   function displayForecast() {
     let forecastElement = document.querySelector("#forecast");
 
-    let days = ["Thu", "Fri", "Sat"];
+    let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
 
 let forecastHTML = `<div class="row">`;
-days.forEach(function(day) {
+days.forEach(function (day) {
   forecastHTML = forecastHTML + `
-            <div class="col-2">
-            <div>${day}</div>
-              <img
-              src="http://openweathermap.org/img/wn/50d@2x.png"
-              alt=""
-              width="42"/>
-              <div>14oC</div>
-            </div>
-      </div>`;
-    
+  <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+        </div>
+      </div>
+  `;
+  });
 forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
-  });
+
 }
  
   
